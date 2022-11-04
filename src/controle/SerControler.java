eac5a3d94949bc869f708fd279d96c3912117469
Enzo <enzo.gr16@aluno.ifsc.edu.br>
2022-11-04 14:30:03 -0300

    commit atualizacao

---
+++ src/controle/SerControler.java
@@ -0,0 +1,30 @@
+package controle;
+
+import java.util.ArrayList;
+
+import modelo.Ser;
+
+public class SerControler {
+	private ArrayList<Ser> ser;
+	
+	public SerControler() {
+		this.ser = new ArrayList<>();
+	}
+	
+	public boolean inserir(Ser s) {
+		return false;
+	}
+	
+	public boolean alterar(Ser s) {
+		return false;
+	}
+	
+	public boolean deletar(Ser s, int idSer) {
+		return false;
+	}
+	
+	public ArrayList<Ser> ser() {
+		return null;
+	}
+
+}
---
+++ src/modelo/Ser.java
@@ -0,0 +1,41 @@
+package modelo;
+
+public class Ser {
+	private boolean isPokemon;
+	private String nome;
+	private double altura;
+	private double peso;
+	private String desc;
+	public boolean isPokemon() {
+		return isPokemon;
+	}
+	public void setPokemon(boolean isPokemon) {
+		this.isPokemon = isPokemon;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	public double getPeso() {
+		return peso;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+	
+}
