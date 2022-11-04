1a3732254ffbafd784daf5b2c19f50caead3ed59
Ezequiel RIbas Figueira Junior <juniorfigueira0800@gmail.com>
2022-11-04 12:07:58 -0300

    commit pokemon

---
+++ src/modelo/Pokemon.java
@@ -0,0 +1,15 @@
+package modelo;
+
+public class Pokemon extends Ser {
+	private  String regiao;
+
+	public String getRegiao() {
+		return regiao;
+	}
+
+	public void setRegiao(String regiao) {
+		this.regiao = regiao;
+	}
+	
+
+}
