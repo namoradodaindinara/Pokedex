89c305069949a80e7660b7f20accd3b16886d5ba
namoradodaindinara <111136070+namoradodaindinara@users.noreply.github.com>
2022-11-04 11:59:38 -0300

    commit treinador

--- src/modelo/Treinador.java
+++ src/modelo/Treinador.java
@@ -1,5 +1,24 @@
 package modelo;
 
-public class Treinador {
+import java.time.LocalDate;
 
+public class Treinador extends Ser {
+	private String cpf;
+	private LocalDate dateNasc;
+	
+	
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public LocalDate getDateNasc() {
+		return dateNasc;
+	}
+	public void setDateNasc(LocalDate dateNasc) {
+		this.dateNasc = dateNasc;
+	}
+	
+	
 }
