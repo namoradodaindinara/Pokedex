566c3599a20108c48e2fcd9c23c470fe94a85f85
JuninRibas <juniorfigueira0800@gmail.com>
2022-11-16 21:47:34 -0300

    commit tela inicio 2.0

--- src/visao/TelaInicio.java
+++ src/visao/TelaInicio.java
@@ -5,6 +5,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TelaInicio extends JFrame {
 
@@ -36,6 +43,30 @@ public class TelaInicio extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Bem Vindo(a) ao");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setForeground(new Color(0, 64, 128));
+		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		lblNewLabel.setBounds(153, 11, 136, 56);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("POKEDEX");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setForeground(new Color(0, 64, 128));
+		lblNewLabel_1.setFont(new Font("Wide Latin", Font.PLAIN, 25));
+		lblNewLabel_1.setBounds(93, 49, 254, 56);
+		contentPane.add(lblNewLabel_1);
+		
+		JButton btnNewButton = new JButton("Cadastrar");
+		btnNewButton.setForeground(new Color(0, 128, 128));
+		btnNewButton.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(119, 124, 206, 68);
+		contentPane.add(btnNewButton);
 	}
-
 }
